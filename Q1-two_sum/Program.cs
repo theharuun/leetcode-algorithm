6196a15e1a91c1ff46704afd1de330b1c306253d
theharuun <160717559+theharuun@users.noreply.github.com>
2024-03-28 22:59:38 +0300

    Add project files.

---
+++ Q1-two_sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_two_sum
+{
+    internal class Program
+    {
+
+        /*
+         TWO SUM
+         Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+         */
+        class Two
+        {
+            public int Addd(int []arr,int target)
+            {
+
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    for(int k = i; k < arr.Length; k++)   //k=i sayesinde aynı datayı tersten yazmadı bir kez okudu diyebiliriz ( arr[i] arr[k] )
+                    {                                                                                                         //( arr[k] arr[i] şeklinde yazılmamasını sağladık)
+                        if (arr[i] + arr[k] == target)
+                        {
+                            
+                            Console.WriteLine("arr["+i+"]  -  " + "arr[" + k + "] ->" + "index are : "+i+" , "+k);
+                        }
+                    }
+                }
+
+
+
+                return 0;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int []arr = { 5, 6, 8, 9, 4 };
+            Two s = new Two();
+            s.Addd(arr, 13);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ leetcode-algorithm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_algorithm
+{
+    internal class Program
+    {
+
+        /*
+         POLINDROME NUMBER
+         Given an integer x, return true if x is a  palindrome , and false otherwise.
+        */
+
+        public class Solution
+        {
+            public bool IsPalindrome(int x)
+            {
+                
+                bool result = false;
+                if (x <= 0)
+                {
+                    Console.WriteLine(result);
+                    return false;
+                }
+                
+                string stringNumber=x.ToString();
+           
+
+                for (int i = 0; i <= stringNumber.Length/2;  i++) 
+                {
+                    if (stringNumber[i] != stringNumber[(stringNumber.Length -1)- i])
+                    {
+                        result = false;
+                    }
+                    else
+                    {
+                      result= true;
+                    }
+
+                }
+
+                if(result==true)
+                {
+                    
+                    Console.WriteLine(result);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                    return false; 
+                }
+               
+
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.IsPalindrome(-1001);
+
+            Console.ReadLine();
+        }
+    }
+}

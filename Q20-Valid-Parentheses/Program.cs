b4a065a7a3cdb0d6e75f707341ed6a671d67396a
theharuun <160717559+theharuun@users.noreply.github.com>
2024-04-08 19:43:31 +0300

    parantez tersi algoritması bitti

---
+++ Q20-Valid-Parentheses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q20_Valid_Parentheses
+{
+    internal class Program
+    {
+
+        public class Solution
+        {
+            public bool IsValid(string s)
+            {
+                while (s.Contains("()") || s.Contains("{}") || s.Contains("[]"))
+                {
+                    s = s.Replace("()", "");
+                    s = s.Replace("{}", "");
+                    s = s.Replace("[]", "");
+                    Console.WriteLine(s);
+                }
+                return s.Length == 0 ? true : false;
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string a = "{}";
+            s.IsValid(a);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}

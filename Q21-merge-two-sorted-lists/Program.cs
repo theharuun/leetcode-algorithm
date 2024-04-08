4345bf0391997789c4f9bbe1bd911693bad4584b
theharuun <160717559+theharuun@users.noreply.github.com>
2024-04-08 20:22:41 +0300

    iki sıralı listeyi birleştirdik bunun için merge fonk var lakin mantığını daha iyi kavramak için yaptım

---
+++ Q21-merge-two-sorted-lists/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q21_merge_two_sorted_lists
+{
+    internal class Program
+    {
+
+
+          public class ListNode {
+               public int val;
+               public ListNode next;
+               public ListNode(int val=0, ListNode next=null) {
+                   this.val = val;
+                   this.next = next;
+               }
+           }
+ 
+        public class Solution
+        {
+            public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+            {
+                // birtane birleştirilmiş node oluşturdum
+                ListNode mergedddd=new ListNode();
+
+                // bir tanede işlemleri üzerinde yapcağımız şimdiki node oluşturduk
+                ListNode current=  mergedddd;
+
+                while(list1!=null && list2!=null)//eğer nodelar boş olmadığı sürece bu döngüyü yap
+                {
+                    if(list1.val<list2.val)        // eğer list2'nin değeri list1'in değerinden büyükse 
+                    {                              // şimdiki nodeun nextine küçük olan node getir(list1) 
+                        current.next = list1;      // küçük olan yerinede küçükten sonrasını getir
+                        list1 = list1.next;
+
+                    }
+                    else                           // else kısmıda ifin tersi
+                    {
+                        current.next = list2;
+                        list2 = list2.next;
+                    }                             
+                    current = current.next;      // burada en son curret hep ilk sayımıza eşitlemiş olduk
+                }
+
+                          // list1 boşsa list2yi yaz değilse list1 yaz şimdikinin nextine 
+                if (list1 != null)
+                {
+                    current.next = list1;
+                }
+                else
+                {
+                    current.next = list2;
+                }
+                return mergedddd;
+            }
+        }
+        static void Main(string[] args)
+        {
+           
+ 
+
+
+        
+        }
+    }
+}

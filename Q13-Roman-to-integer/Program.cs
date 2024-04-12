edc9c71608225d96af2db14ea45ad1effec24495
theharuun <160717559+theharuun@users.noreply.github.com>
2024-04-12 17:13:20 +0300

    plus-one bitti

---
+++ Q13-Roman-to-integer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q13_Roman_to_integer
+{
+    internal class Program
+    {
+        public class Solution
+        {
+           
+                public int RomanToInt(string s)
+                {
+                    int res = 0;
+                    int prev = 0;
+                    Dictionary<char, int> romanNumerals =
+                        new Dictionary<char, int>
+                        {
+                    {'M', 1000},
+                    {'D', 500},
+                    {'C', 100},
+                    {'L' , 50},
+                    {'X' , 10},
+                    {'V' , 5},
+                    {'I' , 1}
+                        };
+                    for (int i = s.Length - 1; i >= 0; i--)
+                    {
+                        if (prev <= romanNumerals[s[i]])
+                        {
+                            res += romanNumerals[s[i]];
+                        }
+                        else if (prev > romanNumerals[s[i]])
+                        {
+                            res -= romanNumerals[s[i]];
+                        }
+                        prev = romanNumerals[s[i]];
+                    }
+                    return res;
+                }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.RomanToInt("IV");
+
+
+          Console.ReadLine();
+        }
+    }
+}
---
+++ Q66-Plus-one/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q66_Plus_one
+{
+    internal class Program
+    {
+
+        /*
+         public class Solution 
+{
+    public int[] PlusOne(int[] digits) 
+        {
+            
+                int toplam = 0;
+                int basamak=(digits.Length-1);
+                for(int i=0;i<digits.Length;i++)
+                {
+                    while(basamak>=0)
+                    {
+                       toplam+= digits[i]*Convert.ToInt16( Math.Pow(10, basamak ));
+                    }
+                }
+                toplam += 1;
+                string toplamStr=toplam.ToString();
+                int[] newDigits=new int[toplamStr.Length];
+
+                for(int i=0;i<newDigits.Length;i++)
+                {
+                    newDigits[i] = int.Parse(toplamStr[i].ToString());
+                }
+
+                Console.WriteLine("Tam Sayının Rakamları:");
+                foreach (int digit in newDigits)
+                {
+                    Console.WriteLine(digit);
+                }
+
+
+                return newDigits;
+            
+        }
+        
+}
+         */
+
+        public class Solution
+        {
+            public int[] PlusOne(int[] digits)
+            {
+                int toplam = 0;
+                int basamak = digits.Length;
+                int[] result = digits;
+                if (digits[digits.Length-1] != 9)
+                {
+                    result[result.Length - 1] += 1;
+
+                    Console.WriteLine("Tam Sayının Rakamları:");
+                    foreach (int digit in result)
+                    {
+                        Console.Write(digit + "-");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < digits.Length; i++)
+                    {
+                        while (basamak >= 0)
+                        {
+                            toplam += digits[i] * Convert.ToInt16(Math.Pow(10, (basamak-1-i)));
+                        }
+                    }
+                    string toplamStr = toplam.ToString();
+                    int[] newDigits = new int[toplamStr.Length+1];
+                    for (int i = 0; i < newDigits.Length; i++)
+                    {
+                        newDigits[i] = int.Parse(toplamStr[i].ToString());
+                    }
+                    // toplam =10 iki digits
+                    Console.WriteLine("/nTam Sayının Rakamları:");
+                    foreach (int digit in newDigits)
+                    {
+                        Console.WriteLine(digit);
+                    }
+
+
+                    {/*
+                        result[result.Length - 2] += 1;
+                        result[result.Length - 1] = 0;
+
+                        Console.WriteLine("Tam Sayının Rakamları:");
+                        foreach (int digit in result)
+                        {
+                            Console.Write(digit + "-");
+                        }
+                    */}
+
+                }
+
+
+                return digits;
+            }
+
+        
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] array = { 1, 2, 3, 4 };
+            s.PlusOne(array);
+            int[] array2 = {9};
+            s.PlusOne(array2);
+            Console.ReadLine();
+        }
+    }
+}

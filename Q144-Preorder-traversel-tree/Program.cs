b23e4880432463f70e61a2a44c08c1246a9b69c1
theharuun <160717559+theharuun@users.noreply.github.com>
2024-05-14 21:15:23 +0300

    Tree Traversals (Inorder, Preorder and Postorder)

---
+++ Q144-Preorder-traversel-tree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q144_Preorder_traversel_tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {   // root -> left -> right   
+            // once kokuk degerini sonra kokun solunu sonra kokun sagindakinin degerini yaziyor
+            IList<int> list = new List<int>();
+            if (root == null) { return null; }
+
+            Console.WriteLine(root.val);
+            PreorderTraversal(root.left);
+            PreorderTraversal(root.right);
+
+
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode root2 = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            PreorderTraversal(root);
+            Console.WriteLine("******************");
+            root2.right = new TreeNode(2);
+            root2.right.left = new TreeNode(3);
+            PreorderTraversal(root2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Q94-Inorder-traversel-tree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q94_Inorder_traversel_tree
+{
+    internal class Program
+    {
+          public class TreeNode
+          { 
+               public int val;
+               public TreeNode left;
+               public TreeNode right;
+               public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+                      {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+              }
+          }
+ 
+       
+            public static IList<int> InorderTraversal(TreeNode root)
+            {     // left -> root -> right
+                  // once kokukun leftinin degerini sonra kokun ,sonra kokun sagindakinin degerini yaziyor
+            IList<int> list = new List<int>();
+                if (root == null) { return null; }
+
+                InorderTraversal(root.left);
+                Console.WriteLine(root.val);
+                InorderTraversal(root.right);
+               
+
+                return list ;
+            }
+        
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode root2 = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            InorderTraversal( root );
+            Console.WriteLine("******************");
+            root2.right = new TreeNode(2);
+            root2.right.left = new TreeNode(3);
+            InorderTraversal(root2);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

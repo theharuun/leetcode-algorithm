4611197221fffe573c7750c53d86ee6465452916
theharuun <160717559+theharuun@users.noreply.github.com>
2024-06-10 14:13:22 +0300

    Q202-happy number sorusu cozuldu , generic yapilar kullanildi.

---
+++ Q169-majority-element/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q169_majority_element
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public static int big;
+            public  int MajorityElement(int[] nums)
+            {
+
+             
+                for (int i = 0;i<nums.Length;i++)
+                {
+                    big = nums[i];
+                    {
+                        for(int j=i+1;j<nums.Length;j++)
+                        {
+                            if (nums[j] > big)
+                            {
+                                big = nums[j];
+                            }
+                           
+                        }
+                    }
+                }
+                return big;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 3,3,4 };
+            Solution solution = new Solution();
+            Console.WriteLine( solution.MajorityElement(nums));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Q202-Happy-number/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q202_Happy_number
+{
+    internal class Program
+    {
+        public  class Solution
+        {
+            public bool IsHappy(int sayi)
+            {
+                List<int>nonHappyNumbers = new List<int>();
+
+                while (sayi> 0)
+                {
+
+                    int toplam = 0;
+                    while (sayi > 0)
+                    {
+                        int basamak = sayi % 10; // Son basamağı al
+                        toplam += basamak * basamak; // toplam yeni sayımız aslında toplama her basamaktaki sayıların karelerinin toplamı ekleniyor
+                        sayi /= 10; // Son basamağı kaldır
+                    }
+
+                    if(nonHappyNumbers.Contains(toplam))  // eğer NONhappynumberin icindeyse false don
+                    {
+                        return false;
+                    }
+                    else    // degilsede NONhappynumberin icinde ekle
+                    {
+                       nonHappyNumbers.Add(toplam);
+                       
+                    }
+
+
+                    if (toplam == 1) //1 e eşitse true don
+                    {
+                        return true;
+                    }
+
+
+                    // false veya true donmedigi durumda yeni sayımızı bu fonksıyonların ıcıne tekrar yollamak ıcın sayıyı elde ettigimiz yeni toplama esitledik
+                    sayi = toplam;
+                }
+
+
+                return false;
+              
+
+            }
+        }
+        static void Main(string[] args)
+        {
+           Solution solution = new Solution();
+           string a= solution.IsHappy(2).ToString();
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}

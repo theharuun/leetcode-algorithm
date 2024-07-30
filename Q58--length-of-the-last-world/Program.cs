cdd47d0fa1044a66640c8668429292e14c74de19
theharuun <160717559+theharuun@users.noreply.github.com>
2024-07-30 10:54:23 +0300

    LengthOfLastWord is done . /son kelimenin harf sayısını döndoren fonksiyonu yazdık

---
+++ Q58--length-of-the-last-world/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q58__length_of_the_last_world
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int LengthOfLastWord(string s)
+            {
+                int count = 0;
+                string sWithnessSpace = s.Trim();
+                for (int i = sWithnessSpace.Length - 1; i >= 0; i--)
+                {
+                    if (sWithnessSpace[i] != ' ')
+                    {
+                        count++;
+                    }
+                    if (sWithnessSpace[i] == ' ')
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(count);
+                return count;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.LengthOfLastWord(" Hello world ");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+  

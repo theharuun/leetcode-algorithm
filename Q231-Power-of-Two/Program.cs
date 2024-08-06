a60586a69b44281e08c60ce6538c4a582a560f98
theharuun <160717559+theharuun@users.noreply.github.com>
2024-08-06 11:58:16 +0300

    power o two is or is not ? done.

---
+++ Q231-Power-of-Two/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q231_Power_of_Two
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public bool IsPowerOfTwo(int n)
+            {
+                List<double> powerTwoList = new List<double>();
+
+                for (double i = 0; i < 100; i++) 
+                {
+                    powerTwoList.Add(Math.Pow(2, i));
+                }
+
+                if (powerTwoList.Contains(n)) 
+                {
+                    Console.WriteLine("true");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                    return false;
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.IsPowerOfTwo(10);
+
+            Console.ReadLine();
+        }
+    }
+}
